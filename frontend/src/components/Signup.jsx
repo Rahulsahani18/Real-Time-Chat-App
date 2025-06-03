bf41d432d3af834824ee11b57cfdaaed61ad0664
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2025-06-03 15:21:00 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8080/api/v1/user/register', user, {
+      const response = await axios.post('/api/v1/user/register', user, {
         headers: {
           'Content-Type': 'application/json'
         },
