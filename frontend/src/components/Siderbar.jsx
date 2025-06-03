84b2661a1b045e15103b1825b400d70725283090
Rahul sahani <153098636+Rahulsahani18@users.noreply.github.com>
2025-06-03 15:33:30 +0530

    Update Siderbar.jsx

--- frontend/src/components/Siderbar.jsx
+++ frontend/src/components/Siderbar.jsx
@@ -20,7 +20,7 @@ const Siderbar = () => {
 
   const logoutHandler = async() => {
     try{
-     const res = await axios.get('http://localhost:8080/api/v1/user/logout');
+     const res = await axios.get('/api/v1/user/logout');
       navigate('/login');
       toast.success('Logged out successfully');
       dispatch(setAuthUser(null)); // clear other users from redux store
